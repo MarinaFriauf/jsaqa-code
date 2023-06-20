6110b26a3febbe327dccc85bf7297bead50d5bd5
Marina Friauf <marina506506506@gmail.com>
2023-06-20 22:53:47 +0200

    задание jest

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -15,3 +15,28 @@ describe("Books names test suit", () => {
     ]);
   });
 });
+
+
+it("should names with duplicate titles", () => {
+  expect(
+    sorting.sortByName([
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ])
+  ).toEqual([
+    "Властелин Колец",
+    "Властелин Колец",
+    "Волшебник изумрудного города",
+    "Гарри Поттер",
+  ]);
+});
+
+it("should return the same array if it contains only one element", () => {
+  expect(sorting.sortByName(["Гарри Поттер"])).toEqual(["Гарри Поттер"]);
+  });
+  
+it("should return an empty array if the input array is empty", () => {
+  expect(sorting.sortByName([])).toEqual([]);
+  });
