e08cb51ad1a3ddc07fd0980dee3c7383b23acbbf
Marina Friauf <marina506506506@gmail.com>
2023-06-25 18:40:31 +0200

    formatted

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -3,7 +3,7 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  }, 10000);
+}, 10000);
 
 afterEach(() => {
   page.close();
@@ -13,6 +13,7 @@ describe("Github page tests", () => {
   beforeEach(async () => {
     await page.goto("https://github.com/team");
   }, 10000);
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
@@ -22,7 +23,7 @@ describe("Github page tests", () => {
   }, 10000);
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    const actual = await page.$eval("a", link => link.getAttribute('href'));
     expect(actual).toEqual("#start-of-content");
   }, 10000);
 
@@ -32,37 +33,37 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toMatch("Get started with Team")
-  },10000);
+    expect(actual).toMatch("Get started with Team");
+  }, 10000);
 });
 
-  describe("Other page explore", () => {
-    beforeEach(async () => {
-      await page.goto("https://github.com/explore");
-    }, 10000);
-    test("The header content Explore", async () => {
-      const title = await page.title();
-      expect(title).toEqual(
-        "Explore GitHub · GitHub"
-      );
-    }, 10000);
-    
-        test("Click on Topics button", async () => {
-          const topicsButton = await page.waitForSelector('a[data-selected-links*="topics_path"]');
-          await topicsButton.click();
-          await page.waitForSelector('.f3.lh-condensed.text-center.Link--primary');
-          const currentUrl = await page.url();
-          expect(currentUrl).toContain("https://github.com/topics");
-        }, 10000);
-      });    
-      describe("Open marketplace page", () => {
-        beforeEach(async () => {
-          await page.goto("https://github.com/marketplace");
-        }, 10000);
-        test("The header content Marketplace", async () => {
-          const title = await page.title();
-          expect(title).toEqual(
-            "GitHub Marketplace · to improve your workflow · GitHub"
-          );
-        }, 10000);  
-  });
+describe("Other page explore", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/explore");
+  }, 10000);
+
+  test("The header content Explore", async () => {
+    const title = await page.title();
+    expect(title).toEqual("Explore GitHub · GitHub");
+  }, 10000);
+
+  test("Click on Topics button", async () => {
+    const topicsButton = await page.waitForSelector('a[data-selected-links*="topics_path"]');
+    await topicsButton.click();
+    await page.waitForSelector('.f3.lh-condensed.text-center.Link--primary');
+    const currentUrl = await page.url();
+    expect(currentUrl).toContain("https://github.com/topics");
+  }, 10000);
+});
+
+describe("Open marketplace page", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/marketplace");
+  }, 10000);
+
+  test("The header content Marketplace", async () => {
+    const title = await page.title();
+    expect(title).toEqual("GitHub Marketplace · to improve your workflow · GitHub");
+  }, 10000);
+});
+
