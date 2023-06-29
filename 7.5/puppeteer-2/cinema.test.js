cebd352b8640f8bb1243760131d334821521f75c
Marina Friauf <marina506506506@gmail.com>
2023-06-29 02:17:37 +0200

    Puppeteer 2

---
+++ 7.5/puppeteer-2/cinema.test.js
@@ -0,0 +1,61 @@
+const { expect } = require("chai");
+const {
+  clickElement,
+  clickOnSelectedPlaceByIndex,
+  getText,
+} = require("./lib/cinemacommands.js");
+
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto("http://qamid.tmweb.ru/client/index.php");
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+describe("Positives Tests ", () => {
+  it("should open reservation on chosen date and time", async () => {
+    const selectForDate = '[class="page-nav__day-number"]';
+    const selectForTime = '[data-seance-id="175"]';
+    const expectText = "Терминатор-заржавел";
+    const expectText2 = "Начало сеанса: 10:00";
+    await page.waitForSelector(selectForDate, { visible: true });
+    await clickElement(page, selectForDate);
+    await clickElement(page, selectForTime);
+    const selectH2 = await getText(page, "h2");
+    const selectTime = await getText(page, '[class="buying__info-start"]');
+    expect(selectH2).to.contain(expectText);
+    expect(selectTime).to.contain(expectText2);
+  });
+
+  it("should get e-ticket", async () => {
+    const selectSecondFilm = '[data-seance-id="175"]';
+    const selectButton = '[class="acceptin-button"]';
+    const acceptButton = '[class="acceptin-button"]';
+    const expectText = "Электронный билет";
+
+    await clickElement(page, selectSecondFilm);
+    await clickOnSelectedPlaceByIndex(page, 0, 0);
+    await clickElement(page, selectButton);
+    await clickElement(page, acceptButton);
+    const textFromPage = await getText(page, '[class="ticket__check-title"]');
+    expect(textFromPage).to.contain(expectText);
+  });
+});
+
+describe("Negative Test Suite", () => {
+  it("should not accept an already booked seat", async () => {
+    const selectFirstFilm = '[data-seance-id="175"]';
+
+    await clickElement(page, selectFirstFilm);
+    await clickOnSelectedPlaceByIndex(page, 0, 0);
+    const selectButton = await page.$(
+      '[class="acceptin-button"][disabled="true"]'
+    );
+    expect(selectButton).to.exist;
+  });
+});
---
+++ 7.5/puppeteer-2/lib/cinemacommands.js
@@ -0,0 +1,41 @@
+module.exports = {
+  clickElement: async function (page, selector) {
+    try {
+      await page.waitForSelector(selector);
+      await page.click(selector);
+    } catch (error) {
+      throw new Error(`Selector is not clickable: ${selector}`);
+    }
+  },
+
+  clickOnSelectedPlaceByIndex: async function (page, rowIndex, placeIndex) {
+    await page.waitForSelector('[class="buying-scheme__wrapper"]'); 
+    const rowSelector = await page.$$('[class="buying-scheme__row"]');
+    const placeSelector = await rowSelector[rowIndex].$$('[class*="buying-scheme__chair"]');
+    try {
+      await placeSelector[placeIndex].click();
+    } catch (error) {
+      throw new Error(`Selector is not clickable: ${placeSelector[placeIndex]}`);
+    }
+  },
+
+  getText: async function (page, selector) {
+    try {
+      await page.waitForSelector(selector);
+      return await page.$eval(selector, (el) => el.textContent);
+    } catch (error) {
+      throw new Error(`Text is not available for selector: ${selector}`);
+    }
+  },
+
+  putText: async function (page, selector, text) {
+    try {
+      const inputField = await page.$(selector);
+      await inputField.focus();
+      await inputField.type(text);
+      await page.keyboard.press("Enter");
+    } catch (error) {
+      throw new Error(`Not possible to type text for selector: ${selector}`);
+    }
+  },
+};
